1f78cd79d49a5d4f19c9f9c792890274de696638
Konstantin Münster <konstantin.muenster@mail.com>
2019-07-21 19:47:22 +0200

    Updates

---
+++ precache-manifest.0e5e98f9fa488528332b6c7be66bf746.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad9eede7bb8b77ab3625561aada28f7",
+    "url": "/simple-react-js-weather-app/index.html"
+  },
+  {
+    "revision": "9e150933699f9516058a",
+    "url": "/simple-react-js-weather-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "90f6cfdfd30cb5e651c3",
+    "url": "/simple-react-js-weather-app/static/css/main.286833ee.chunk.css"
+  },
+  {
+    "revision": "9e150933699f9516058a",
+    "url": "/simple-react-js-weather-app/static/js/2.a9cbcb86.chunk.js"
+  },
+  {
+    "revision": "90f6cfdfd30cb5e651c3",
+    "url": "/simple-react-js-weather-app/static/js/main.5792caf4.chunk.js"
+  },
+  {
+    "revision": "6f8c748deb2efc673da1",
+    "url": "/simple-react-js-weather-app/static/js/runtime~main.bf3a8b8b.js"
+  },
+  {
+    "revision": "f963a166b3279d455437165cb78cce6a",
+    "url": "/simple-react-js-weather-app/static/media/Clear.f963a166.svg"
+  },
+  {
+    "revision": "a82005aa954c61cba1a50336159d70d8",
+    "url": "/simple-react-js-weather-app/static/media/Cloud-wind.a82005aa.svg"
+  },
+  {
+    "revision": "a98369fb7ac4ac2d522c92642bb69bc8",
+    "url": "/simple-react-js-weather-app/static/media/Clouds.a98369fb.svg"
+  },
+  {
+    "revision": "1ca35fb1145fa93cb66fa81fc2b9da72",
+    "url": "/simple-react-js-weather-app/static/media/Drizzle.1ca35fb1.svg"
+  },
+  {
+    "revision": "e3158b56e90d7c4ae2ac85e0ef7ed4bc",
+    "url": "/simple-react-js-weather-app/static/media/Moon.e3158b56.svg"
+  },
+  {
+    "revision": "06f78ffe921aff71ae391ae110b95c08",
+    "url": "/simple-react-js-weather-app/static/media/Night-rainy.06f78ffe.svg"
+  },
+  {
+    "revision": "7d9c9233f8d2a0402a90753501bfd21e",
+    "url": "/simple-react-js-weather-app/static/media/Night.7d9c9233.svg"
+  },
+  {
+    "revision": "29a0aca0c7b11b9e2e20e203bca66089",
+    "url": "/simple-react-js-weather-app/static/media/Preview.29a0aca0.svg"
+  },
+  {
+    "revision": "b5e8855f869aeaa037290f87cf347fd8",
+    "url": "/simple-react-js-weather-app/static/media/Rain.b5e8855f.svg"
+  },
+  {
+    "revision": "104ea80ade6e3fd46928a77892b6d86c",
+    "url": "/simple-react-js-weather-app/static/media/Snow.104ea80a.svg"
+  },
+  {
+    "revision": "c623741c0b9e188e5b6a93a5e1c0ad9e",
+    "url": "/simple-react-js-weather-app/static/media/Thunderstorm.c623741c.svg"
+  },
+  {
+    "revision": "cd8d7fceea994a098c78b381e4e9916a",
+    "url": "/simple-react-js-weather-app/static/media/Tonado.cd8d7fce.svg"
+  }
+]);
