d3b5b381fde5550e76bf3c3531983e845e3246e9
Konstantin Münster <konstantin.muenster@mail.com>
2019-07-21 19:54:19 +0200

    Minor fixes

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -44,7 +44,7 @@ class App extends Component {
   setWeather = () => {
     const city = this.state.searchBarInput;
     const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-    const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     this.setState({weatherDetails: {}, loading: true, error: false}, () => {
       fetch(URL)
         .then(res => res.json())
